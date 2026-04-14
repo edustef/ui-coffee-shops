2cdcf2840a2c1e4cacd7704610866c9f9e72d679
edustef <eduardstefan290@gmail.com>
2026-04-14 17:54:06 +0300

    feat: added react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import babelPlugin from '@rolldown/plugin-babel'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), babelPlugin({ presets: [reactCompilerPreset()] }), tailwindcss()],
 })
