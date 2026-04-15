54de6a6805664d04c5686515ea7740c10a283ac3
edustef <eduardstefan290@gmail.com>
2026-04-15 09:47:43 +0300

    feat: added coffee shop screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
+import { ShopList } from './components/shopList'
+
+import { Filters } from './components/filters'
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50 p-6">
-      <h1 className="text-2xl font-bold text-gray-900 mb-6">Coffee Shops</h1>
-      {/* TODO: build your UI here */}
+    <div className="min-h-screen bg-gray-50 px-4 py-6 md:p-6">
+      <h1 className="mt-6 md:mt-12 text-center text-3xl md:text-5xl font-bold text-gray-900 mb-6">
+        Coffee Addicts
+      </h1>
+
+      <div className="mt-6 md:mt-12 mx-auto max-w-7xl flex flex-col md:flex-row gap-8 md:gap-12 w-full md:justify-around">
+        {/* Filters */}
+        <div className="w-full md:max-w-xs">
+          <h2 className="font-bold text-2xl">Filter</h2>
+          <Filters />
+        </div>
+
+        {/* Shops */}
+        <div className="w-full md:max-w-md">
+          <h2 className="font-bold text-2xl">Shops</h2>
+          <ShopList className="mt-4" />
+        </div>
+      </div>
     </div>
   )
 }
