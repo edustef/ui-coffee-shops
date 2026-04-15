ead95461e07be186e45a7094f91336390402ea8c
edustef <eduardstefan290@gmail.com>
2026-04-15 09:23:26 +0300

    feat: added indie flower font

--- src/components/shopItem.tsx
+++ src/components/shopItem.tsx
@@ -9,9 +9,9 @@ interface Props {
 
 export function ShopItem({ name, position, distance, isHighlighted }: Props) {
   return (
-    <li className={`mb-4 p-4 rounded ${isHighlighted ? 'text-blue-500' : 'text-black'}`}>
+    <li className={`p-4 rounded ${isHighlighted ? 'text-blue-500' : 'text-black'}`}>
       <h2 className="text-xl font-semibold">{name}</h2>
-      <p>
+      <p className="text-lg">
         {position.x}, {position.y} {distance && `- ${distance}`}
       </p>
     </li>
