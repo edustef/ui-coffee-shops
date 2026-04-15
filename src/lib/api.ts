8d8bacf27ae64d21078ec219c1c6c01dedfd4b87
edustef <eduardstefan290@gmail.com>
2026-04-15 08:39:45 +0300

    refactor: transform api data to desired output

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,43 +1,56 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 export const positionSchema = z.object({
   x: z.coerce.number({ message: 'x must be a number' }),
   y: z.coerce.number({ message: 'y must be a number' }),
-});
+})
 
-export const shopsSchema = z.array(positionSchema.extend({
-  id: z.number(),
-  name: z.string(),
-  created_at: z.iso.datetime(),
-  updated_at: z.iso.datetime(),
-}));
+export const shopsSchema = z.array(
+  positionSchema
+    .extend({
+      id: z.number(),
+      name: z.string(),
+      created_at: z.iso.datetime(),
+      updated_at: z.iso.datetime(),
+    })
+    .transform((shop) => ({
+      id: shop.id,
+      name: shop.name,
+      createdAt: shop.created_at,
+      updatedAt: shop.updated_at,
+      position: {
+        x: shop.x,
+        y: shop.y,
+      },
+    })),
+)
 
+export type Shop = z.infer<typeof shopsSchema>[number]
 
 export async function fetchShops() {
-  const url = 'https://api-challenge.agilefreaks.com/v1/coffee_shops';
-  const apiToken = await getToken();
-  const urlWithToken = `${url}?token=${apiToken}`;
+  const url = 'https://api-challenge.agilefreaks.com/v1/coffee_shops'
+  const apiToken = await getToken()
+  const urlWithToken = `${url}?token=${apiToken}`
 
   const response = await fetch(urlWithToken, {
-    headers: { 'Accept': 'application/json' },
-  });
+    headers: { Accept: 'application/json' },
+  })
 
-  const data = await response.json();
-  return shopsSchema.parse(data);
+  const data = await response.json()
+  return shopsSchema.parse(data)
 }
 
-
 export async function getToken() {
-  const url = 'https://api-challenge.agilefreaks.com/v1/tokens';
+  const url = 'https://api-challenge.agilefreaks.com/v1/tokens'
   const response = await fetch(url, {
     method: 'POST',
-    headers: { 'Accept': 'application/json' },
-  });
+    headers: { Accept: 'application/json' },
+  })
 
   if (!response.ok) {
-    throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+    throw new Error(`HTTP ${response.status}: ${response.statusText}`)
   }
 
-  const data = await response.json();
-  return data.token;
+  const data = await response.json()
+  return data.token
 }
